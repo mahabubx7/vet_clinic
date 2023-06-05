a5ac1c9f4ab609ad9f68cc4bbdb3385f8355b08d
Mahabub Alam <mahabubx7@gmail.com>
2023-06-05 19:37:35 +0600

    :heavy_check_mark: added-animals-psql

--- schema.sql
+++ schema.sql
@@ -1,5 +1,10 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    name varchar(100)
+    id int not null,
+    name varchar(100) not null,
+    date_of_birth date not null,
+    escape_attempts int,
+    neutered boolean,
+    weight_kg decimal not null
 );
