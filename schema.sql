7b742dc8d212619aa0871caad4c70de1ceae7664
Mahabub Alam <mahabubx7@gmail.com>
2023-06-05 20:18:32 +0600

    :hammer: Fix(missing): id update property rule as Database had.

--- schema.sql
+++ schema.sql
@@ -1,10 +1,11 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    id int not null,
+    id serial PRIMARY KEY,
     name varchar(100) not null,
     date_of_birth date not null,
     escape_attempts int,
     neutered boolean,
     weight_kg decimal not null
 );
+
