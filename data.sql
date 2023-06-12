433dddbaa481b06eeaf32c3b0836e269844084b3
Mahabub Alam <mahabubx7@gmail.com>
2023-06-13 04:44:48 +0600

    :white_check_mark: Changes(>=1000): updated & new screenshots

--- data.sql
+++ data.sql
@@ -52,18 +52,18 @@ UPDATE animals SET species_id = 1 WHERE species_id IS NULL;
 COMMIT;
 SELECT * FROM animals;
 
--- modify owners_id
+-- modify owner_id
 BEGIN;
-UPDATE animals SET owners_id = 1 WHERE name IN('Agumon');
-UPDATE animals SET owners_id = 2 WHERE name IN('Gabumon', 'Pikachu');
-UPDATE animals SET owners_id = 3 WHERE name IN('Devimon', 'Plantmon');
-UPDATE animals SET owners_id = 4 WHERE name IN('Charmander', 'Squirtle','Blossom');
-UPDATE animals SET owners_id = 5 WHERE name IN('Angemon', 'Boarmon');
+UPDATE animals SET owner_id = 1 WHERE name IN('Agumon');
+UPDATE animals SET owner_id = 2 WHERE name IN('Gabumon', 'Pikachu');
+UPDATE animals SET owner_id = 3 WHERE name IN('Devimon', 'Plantmon');
+UPDATE animals SET owner_id = 4 WHERE name IN('Charmander', 'Squirtle','Blossom');
+UPDATE animals SET owner_id = 5 WHERE name IN('Angemon', 'Boarmon');
 COMMIT;
 SELECT * FROM animals;
 
 -- insert into vets
-INSERT INTO vets(name, age, graduation_date)
+INSERT INTO vets(name, age, date_of_graduation)
 VALUES('William Tatcher', 45, '2000-04-23'),
 ('Maisy Smith', 26, '2019-01-17'),
 ('Stephanie Mendez', 64, '1981-05-04'),
@@ -77,7 +77,7 @@ VALUES((SELECT id FROM species WHERE name = 'Pokemon'),(SELECT id FROM vets WHER
 ((SELECT id FROM species WHERE name = 'Digimon'),(SELECT id FROM vets WHERE name = 'Jack Harkness'));
 
 -- insert into visits
-INSERT INTO visits(animal_id, vet_id, date)
+INSERT INTO visits(animal_id, vet_id, date_of_visit)
 VALUES((SELECT id FROM animals WHERE name = 'Agumon'), (SELECT id FROM vets WHERE name = 'William Tatcher'), '2020-05-24'),
 ((SELECT id FROM animals WHERE name = 'Agumon'), (SELECT id FROM vets WHERE name = 'Stephanie Mendez'), '2020-07-22'),
 ((SELECT id FROM animals WHERE name = 'Gabumon'), (SELECT id FROM vets WHERE name = 'Jack Harkness'), '2021-02-02'),
@@ -99,10 +99,3 @@ VALUES((SELECT id FROM animals WHERE name = 'Agumon'), (SELECT id FROM vets WHER
 ((SELECT id FROM animals WHERE name = 'Blossom'), (SELECT id FROM vets WHERE name = 'Stephanie Mendez'), '2020-05-24'),
 ((SELECT id FROM animals WHERE name = 'Blossom'), (SELECT id FROM vets WHERE name = 'William Tatcher'), '2021-01-11');
 
-
-
--- This will add 3.594.280 visits considering you have 10 animals, 4 vets, and it will use around ~87.000 timestamps (~4min approx.)
-INSERT INTO visits (animal_id, vet_id, date) SELECT * FROM (SELECT id FROM animals) animal_ids, (SELECT id FROM vets) vets_ids, generate_series('1980-01-01'::timestamp, '2021-01-01', '4 hours') visit_timestamp;
-
--- This will add 2.500.000 owners with full_name = 'Owner <X>' and email = 'owner_<X>@email.com' (~2min approx.)
-insert into owners (full_name, email) select 'Owner ' || generate_series(1,2500000), 'owner_' || generate_series(1,2500000) || '@mail.com';
